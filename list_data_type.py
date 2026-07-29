d07cf53448ac2e2796e2d454a4914a3de8656b9e
Rahmankhattak711 <rahmankhattak796@gmail.com>
2026-07-30 00:08:02 +0500

    feat: add list and dictionary data type modules and update README documentation

---
+++ list_data_type.py
@@ -0,0 +1,18 @@
+color = ["red", "green", "blue"]
+
+color.insert(1, "yellow")
+
+colors = color.copy()
+
+color.insert(2, "black")
+
+color.remove("yellow")
+
+color.pop()
+
+# color.clear()  # clear the list
+
+
+print(colors)
+
+print(color)
