362763cea54fe3233b923a4da78b61b69f8077d2
Rahmankhattak711 <rahmankhattak796@gmail.com>
2026-08-03 01:03:59 +0500

    feat: add recursive factorial function with print statements for calculation steps

--- advanced/04_function.py
+++ advanced/04_function.py
@@ -6,3 +6,13 @@ def func_arguments(*a, **b):
 func_arguments(1, 2, 3, a=4, b=5)
 
 
+# recursive function
+def factorial(n):
+    print("Calculating factorial of", n)
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+
+print(factorial(5))
