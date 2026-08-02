f8ffc33cb2df28d70614b54508ce2078eae669b7
Rahmankhattak711 <rahmankhattak796@gmail.com>
2026-08-02 23:07:49 +0500

    fix: correct indentation in sales data report functions

--- advanced/01_function.py
+++ advanced/01_function.py
@@ -1,18 +1,23 @@
 # fetch sales and generaete a report
 
+
 def fetch_sales_data():
-  return print("Fetching sales data...")
+    return print("Fetching sales data...")
+
 
 def genereate_monthly_report():
-  return print("Generating monthly report...")
+    return print("Generating monthly report...")
+
 
 def generate_annual_report():
-  return print("Generating annual report...")
+    return print("Generating annual report...")
+
 
 def report_generator():
-  fetch_sales_data()
-  genereate_monthly_report()
-  generate_annual_report()
-  print("Report generation completed.")
+    fetch_sales_data()
+    genereate_monthly_report()
+    generate_annual_report()
+    print("Report generation completed.")
+
 
 report_generator()
